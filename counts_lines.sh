d532e8e67ff2c5024ddbc5aa1eb1885653504dea
Oliver Claas <oclaas@th-owl.de>
2022-11-25 13:43:29 +0100

    Initial commit

---
+++ counts_lines.sh
@@ -0,0 +1,8 @@
+# counts_lines.sh
+# 2022 - Konrad Förstner
+# Counts the lines of all TSV file
+# in the current folder.
+
+echo "Count all lines of TSV files"
+wc -l *tsv
+echo "Counting done - have a nice day!"
